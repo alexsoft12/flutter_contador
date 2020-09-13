3daf395850de7405bbb7bd82863963ab4d994c5d
Yuler Figueroa <yuleralex1@gmail.com>
2020-09-13 11:03:22 -0500

    Iteracion con botones

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -7,6 +7,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _stiloTexto = TextStyle(fontSize: 25.0);
+  int _conteo = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +16,35 @@ class _HomePageState extends State<HomePage> {
         title: Text('Contador'),
       ) ,
       body: Center(
-        child: Text('Hola mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de clicks:', style: _stiloTexto,),
+            Text('$_conteo', style: _stiloTexto,),
+          ],
+        ),
       ),
+      floatingActionButton: _crearBotones(),
     );
   }
+
+ Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0,),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _reset,),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _sustraer),
+        SizedBox(width: 30.0,),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar),
+      ],
+    );
+ }
+
+  void _reset() => setState(() => _conteo = 0);
+
+  void _sustraer() => setState(() => _conteo--);
+
+  void _agregar() => setState(() => _conteo++);
 }
